59cafc95258e3b7379dfa5a09f48267c4f67c33c
delishaandr <13519133@std.stei.itb.ac.id>
2023-06-14 18:23:31 +0700

    add search bar

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,20 @@ function App() {
   return (
     <div className="App">
       <div></div>
-      <div class="container-fluid">
-        <h1 class="display-3 p-2 mt-4 mb-4">News</h1>
-        <div class="card-list p-2">
+      <div class="container-fluid p-4">
+        <h1 class="display-3 mt-4 mb-4">News</h1>
+        <div class="input-group">
+          <input class="form-control form-control-lg" type="text" placeholder="Search" id="search" />
+          <button type="button" class="btn bg-transparent" style={{marginLeft : '-40px', zIndex : '100'}}>
+            <i class="fa fa-times"></i>
+          </button>
+          <div class="input-group-append">
+            <button class="btn btn-secondary" type="button">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-list mt-4">
         {data &&
           data.articles.map(({ id, url, author, title }) => (
             <a href={url} class="card mb-3" target="_blank" key={id}>
