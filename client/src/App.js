6ccc85d11da523e9651ae16d848f08ce8bbfd33f
delishaandr <13519133@std.stei.itb.ac.id>
2023-06-14 17:57:37 +0700

    display data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  const [ data, setData ] = useState(null);
+
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/top-headlines?country=id&apiKey=24173b501c0f4b20aa1f94625417dce0')
+      .then((response) => response.json())
+      .then((actualData) => setData(actualData));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div></div>
+      <div class="container-fluid">
+        <h1 class="display-3 p-2 mt-4 mb-4">News</h1>
+        <div class="card-list p-2">
+        {data &&
+          data.articles.map(({ id, url, author, title }) => (
+            <a href={url} class="card mb-3" target="_blank" key={id}>
+              <div class="card-body">
+                <p class="card-text text-dark">{author}</p>
+                <h5 class="card-title text-dark">{title}</h5>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-// server/index.js
-
 const express = require("express");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.get('/', (req, res) => { 
+ res.send("<h1>Home page</h1>");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
